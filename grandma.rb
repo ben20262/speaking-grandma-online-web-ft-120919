c70174dc20596654129e9a50ac93a63a6ef54678
Benjamin Small <ben20262@aim.com>
2019-12-10 00:47:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,8 @@ def speak_to_grandma(words)
   if words == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif words == words.upcase
-    "NO, NOT SICE 1938!"
+    "NO, NOT SINCE 1938!"
   else
-    "HUH?! SPEAK UP SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
